5235f4cdf0815415d876330db6ff9ba56164a48a
mayorr-star <immaboat11@gmail.com>
2024-06-15 18:01:18 +0000

    Initial commit

---
+++ src/modules/mergeSort.js
@@ -0,0 +1,21 @@
+const mergeArrays = (leftHalf, rightHalf) => {
+  const mergedArray = [];
+  while (leftHalf.length > 0 && rightHalf.length > 0) {
+    if (leftHalf[0] < rightHalf[0]) {
+      mergedArray.push(leftHalf.shift());
+    } else {
+      mergedArray.push(rightHalf.shift());
+    }
+  }
+  return mergedArray.concat(leftHalf, rightHalf);
+};
+
+const sortArray = (arr) => {
+  if (arr.length === 1) return arr[0];
+  const midpoint = Math.floor(arr.length / 2);
+  const leftHalf = sortArray(arr.slice(0, midpoint));
+  const rightHalf = sortArray(arr.slice(midpoint + 1));
+  return mergeArrays(leftHalf, rightHalf);
+};
+
+module.exports = sortArray;
