2a7acb9e81a61f94e58b4ffc77f1920f7255d7be
mayorr-star <immaboat11@gmail.com>
2024-06-16 14:53:27 +0000

    Change node.left and node.right attributes to node.leftChild and node.rightChild
    left and right are not present in the Node class hence when used throw an array stating left and right not defined

--- src/modules/prettyPrint.js
+++ src/modules/prettyPrint.js
@@ -2,12 +2,12 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
     return;
   }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  if (node.rightChild !== null) {
+    prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
   }
   console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  if (node.leftChild !== null) {
+    prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
 };
 
