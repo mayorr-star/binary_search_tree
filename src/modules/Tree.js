72cc76baab47f64817f3b4260f306394370cfa86
mayorr-star <immaboat11@gmail.com>
2024-06-16 16:38:55 +0000

    Create functiom to build BST from a sorted array

--- src/modules/Tree.js
+++ src/modules/Tree.js
@@ -1,48 +1,22 @@
-const Node = require("./node");
 const mergeSort = require("./mergeSort");
+const Node = require("./node");
 
 module.exports = class Tree {
-  constructor(arr) {
-    this.root = this.buildTree(arr);
+  constructor() {
+    this.root = null
   }
 
-  buildTree(arr) {
-    const newArr = this.removeDuplicates(arr);
-    const sortedArray = mergeSort(newArr);
-    return this.buildBST(arr)
+  buildTree(array) {
+    if (array.length === 0) return null;
+    const midpoint = parseInt(array.length / 2);
+    const node = new Node(array[midpoint]);
+    node.leftChild = this.buildTree(array.slice(0, midpoint));
+    node.rightChild = this.buildTree(array.slice(midpoint + 1));
+    return node
   }
-
-  buildPreorderBST(arr, start = 0, end = arr.length - 1) {
-    if (start > end) return null;
-    const midpoint = Math.floor((start + end) / 2);
-    const node = new Node(arr[midpoint]);
-    node.leftChild = this.buildBST(arr, 0, midpoint - 1);
-    node.rightChild = this.buildBST(arr, midpoint + 1, arr.length - 1);
-    return node;
-  }
-
+  
   removeDuplicates(arr) {
-    const newArr = [];
-    for (const element of arr) {
-      if (!newArr.includes(element)) newArr.push(element);
-    }
-    return newArr;
+    return Array.from(new Set(arr));
   }
-
-  insert(value) {
-    if (!this.root) {
-      root = new Node(value);
-      return this.root;
-    } else {
-      if (value < this.root.data) {
-        this.root.leftChild = this.insert(value)
-      } else if (value > this.root.data) {
-        this.root.rightChild = this.insert(value);
-      }
-      return this.root;
-    }
-  }
+  
 }
-const newTree = new Tree([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-console.log(newTree.buildTree([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-// console.log(newTree.buildBST([1, 2 , 3, 4, 5]))
