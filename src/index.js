621732eba94e8c46a8fafd0fb9737335dc71cfbf
mayorr-star <immaboat11@gmail.com>
2024-06-19 19:25:52 +0000

    Declare function to return an array of random numbers less than 100

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 const Tree = require("./modules/Tree");
 const prettyPrint = require("./modules/prettyPrint");
+
+const generateRandomNumbers = (size) => {
+    const randomNumsarray = [];
+    while (randomNumsarray.length < size) {
+        const randomNumber = Math.floor(Math.random() * 100)
+        randomNumsarray.push(randomNumber);
+    }
+    return randomNumsarray
+}
+generateRandomNumbers(12)
