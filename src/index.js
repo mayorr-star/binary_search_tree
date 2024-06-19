6c2d298f96427569c6ef94830a2cb01dd27d3147
mayorr-star <immaboat11@gmail.com>
2024-06-19 19:55:01 +0000

    Insert several numbers greater than 100 into the tree
    To unbalance the tree

--- src/index.js
+++ src/index.js
@@ -2,23 +2,38 @@ const Tree = require("./modules/Tree");
 const prettyPrint = require("./modules/prettyPrint");
 
 const generateRandomNumbers = (size) => {
-    const randomNumsarray = [];
-    while (randomNumsarray.length < size) {
-        const randomNumber = Math.floor(Math.random() * 100)
-        randomNumsarray.push(randomNumber);
-    }
-    return randomNumsarray
-}
+  const randomNumsarray = [];
+  while (randomNumsarray.length < size) {
+    const randomNumber = Math.floor(Math.random() * 100);
+    randomNumsarray.push(randomNumber);
+  }
+  return randomNumsarray;
+};
 console.log("Building a balanced binary search tree.");
 const tree = new Tree(generateRandomNumbers(12));
-prettyPrint(tree.root)
+prettyPrint(tree.root);
 console.log("Confirming if tree is balanced.");
 console.log(`Is tree balanced? ${tree.isBalanced()}.`);
-console.log(`Printing elements in level order`)
-console.log(tree.levelOrder())
-console.log(`Printing elements in preoder order`)
-console.log(tree.preOrder())
-console.log(`Printing elements in post order`)
-console.log(tree.postOrder())
-console.log(`Printing elements in inorder`)
-console.log(tree.inOrder())
+console.log(`Printing elements in level order`);
+console.log(tree.levelOrder());
+console.log(`Printing elements in preoder order`);
+console.log(tree.preOrder());
+console.log(`Printing elements in post order`);
+console.log(tree.postOrder());
+console.log(`Printing elements in inorder`);
+console.log(tree.inOrder());
+console.log("Unbalancing tree by adding several numbers greater than 100");
+tree.insert(150)
+tree.insert(190)
+tree.insert(200)
+tree.insert(210)
+tree.insert(1000)
+tree.insert(340)
+tree.insert(234)
+tree.insert(324)
+tree.insert(460)
+tree.insert(569)
+tree.insert(567)
+tree.insert(956)
+tree.insert(812)
+prettyPrint(tree.root);
