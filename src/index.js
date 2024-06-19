aca789ac43ef4798e53dd70fa5dd5f2048a5caff
mayorr-star <immaboat11@gmail.com>
2024-06-19 19:49:38 +0000

    Print all elements in level order, preorder, postorder and inorder traversal

--- src/index.js
+++ src/index.js
@@ -9,6 +9,16 @@ const generateRandomNumbers = (size) => {
     }
     return randomNumsarray
 }
+console.log("Building a balanced binary search tree.");
 const tree = new Tree(generateRandomNumbers(12));
 prettyPrint(tree.root)
-console.log(tree.isBalanced())
+console.log("Confirming if tree is balanced.");
+console.log(`Is tree balanced? ${tree.isBalanced()}.`);
+console.log(`Printing elements in level order`)
+console.log(tree.levelOrder())
+console.log(`Printing elements in preoder order`)
+console.log(tree.preOrder())
+console.log(`Printing elements in post order`)
+console.log(tree.postOrder())
+console.log(`Printing elements in inorder`)
+console.log(tree.inOrder())
